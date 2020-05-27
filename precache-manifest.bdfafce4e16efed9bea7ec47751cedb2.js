369894258312f4f97dddba3dd4973c18e4abd9d7
Demyan Rogozhin <Demyan.Rogozhin@gmail.com>
2020-05-27 03:41:30 +0200

    Updates

---
+++ precache-manifest.bdfafce4e16efed9bea7ec47751cedb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6009dbc169ceafe0003dd43c8da486c4",
+    "url": "/devolut-frontent/index.html"
+  },
+  {
+    "revision": "d0a613e1cb744d616054",
+    "url": "/devolut-frontent/static/css/main.ae80000b.chunk.css"
+  },
+  {
+    "revision": "140d805efa9fa3a5d061",
+    "url": "/devolut-frontent/static/js/2.b2ba427f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/devolut-frontent/static/js/2.b2ba427f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0a613e1cb744d616054",
+    "url": "/devolut-frontent/static/js/main.b6c0f3cc.chunk.js"
+  },
+  {
+    "revision": "031f4207e10fb32b8524",
+    "url": "/devolut-frontent/static/js/runtime-main.e44ca5a2.js"
+  }
+]);
